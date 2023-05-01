78273f9b6a2dd83d282a0886a7b43f45eb942361
kalharko <oscar.dewasmes@gmail.com>
2023-05-01 23:58:56 +0200

    :tada: start of unicurses interface

--- model/cli/cli_app.py
+++ model/cli/cli_app.py
@@ -1,276 +1,148 @@
-from termcolor import colored as c
+import unicurses as curses
+from logging import raiseExceptions
 
-from model.business_model.game_manager import GameManager
-from model import TokenArray
+from model.game_manager import GameManager
+from model.token_array import TokenArray
+from model.cli.objects_win import PatronWin, CardWin, PlayerWin, InputWin
 
 
 class CliApp():
     def __init__(self, nbPlayer: int) -> None:
         # game
         self.gm = GameManager(nbPlayer)
-        self.gm.launch_game(4)
+        self.gm.launch_game(nbPlayer)
 
         # constants
         self.ESCAPE = ['q']
         self.SCREEN_HEIGHT = 47
-        self.COMMANDS = [
-            'token3',
-            'token2',
-            'reserve',
-            'buyshop',
-            'buyreserved',
-            'displayreserved',
-            'h', 'help'
-        ]
+        self.MIN_SIZE = (23, 67)
+
+        self.COMMANDS = {
+            'take': self.take_tokens,
+            'token': self.take_tokens,
+            'white': self.take_tokens,
+            'blue': self.take_tokens,
+            'green': self.take_tokens,
+            'red': self.take_tokens,
+            'black': self.take_tokens,
+
+            'reserve': self.reserve,
+            'buy': self.buy,
+            'h': self.help,
+            'help': self.help,
+        }
 
         # screen
-        self.width = 80
-        self.height = 24
+        self.stdscr = curses.initscr()
+        self.screenH, self.screenW = curses.getmaxyx(self.stdscr)
+        if self.screenH < self.MIN_SIZE[0] or self.screenW < self.MIN_SIZE[1]:
+            print('console too small' + str((self.screenH, self.screenW)))
+            quit()
+        self.inputWin = InputWin()
+
+        # main loop
         self.main_loop()
 
+        # exit
+        curses.werase(self.stdscr)
+        curses.refresh()
+
     def main_loop(self) -> None:
-        # TODO: while loop
-        boardState = self.gm.gather_cli_board_state()
-        self.display(boardState)
-        user_input = ''
-
-        while not user_input in self.ESCAPE:
+        while True:
+            self.display()
             user_input = self.get_input()
-            command = user_input.split(' ')[0]
-            arguments = user_input.split(' ')[1:]
-            if not command in self.COMMANDS:
-                print('invalid command')
-                continue
-
-            if command in ['help', 'h']:
-                self.print_help()
-                continue
-            if command == 'token3':
-                tokens = [1 if x in arguments else 0 for x in [
-                    'white', 'blue', 'green', 'red', 'black']] + [0]
-                if err := self.gm.take_token(TokenArray(tokens)):
-                    print(err)
-                    break
-                else:
-                    self.nextPlayer()
-            elif command == 'token2':
-                tokens = [2 if x in arguments else 0 for x in [
-                    'white', 'blue', 'green', 'red', 'black']] + [0]
-                tokens = TokenArray(tokens)
-                if not self.gm.take_token(tokens):
-                    pass
-            elif command == 'reserve':
-                cardId = self.gm._shopController.ranks[int(
-                    arguments[0])].hand.cards[int(arguments[1])].card_id
-                if not self.gm.reserve_card(cardId):
-                    pass
-            elif command == 'buyshop':
-                cardId = self.gm._shopController.ranks[int(
-                    arguments[0])].hand.cards[int(arguments[1])].card_id
-                if not self.gm.buy_card(cardId):
-                    pass
-            elif command == 'buyreserved':
-                cardId = self.gm._playerController.players[self.currentPlayer].reserved.cards[int(
-                    arguments[0])].card_id
-                if not self.gm.buy_card(cardId):
-                    pass
-            elif command == 'displayreserved':
-                if boardState['player-nbReserved'] == 0:
-                    print('No reserved cards')
-                    continue
-
-                print('Reserved cards :')
-                for i in boardState['player-nbReserved']:
-                    print(self.str_first_card_price(boardState['player-reserved'][i][0])
-                          + self.str_scnd_card_price(boardState['player-reserved'][i][0])
-                          + ' ' + self.str_card_bonus(boardState['player-reserved'][i][1]
-                          + ' PV: ' + str(boardState['player-reserved'][i][2])))
-                continue
-
-            boardState = self.gm.gather_cli_board_state()
-            self.display(boardState)
-
-    def get_input(self) -> str:
-        return input('=> ')
-
-    def print_help(self) -> None:
-        print("+-- Commands : take3 color,color,color                          --+")
-        print("+--            take2 color                                      --+")
-        print("+--            reserve rank, x                                  --+")
-        print("+--            buyshop rank, x                                  --+")
-        print("+--            buyreserved x                                    --+")
-        print("+--            displayreserved                                  --+")
-
-    def display(self, boardState) -> None:
-        print((self.SCREEN_HEIGHT-2) * "\n")
-
-        bs = boardState
-        print("+---------+----------------------------------+--------------------+")
-
-        print("| PATRONS |              BANK                | CPU#1        VP:", end='')
-        print(str(bs['cpu1-vp']) + (3 - len(str(bs['cpu1-vp']))) * ' ' + '|')
-
-        print("|         |  white blue  green  red  black   | " +
-              self.str_token_array(bs['cpu1-tokens']) + " |")
-
-        print(
-            f"|[- 3PV -]|   ({bs['bank'][0]})   ({bs['bank'][1]})   ({bs['bank'][2]})   ({bs['bank'][3]})   ({bs['bank'][4]})    |", end="")
-        print(
-            " " + self.str_bonus_array(bs['cpu1-bonus']) + f" [{bs['cpu1-nbReserved']}]|")
-
-        print(
-            f"|{self.str_patron_price(bs['patrons'][0])}|                                  +--------------------+")
-
-        print("|         |              SHOP                | CPU#1        VP:", end='')
-        print(str(bs['cpu2-vp']) + (3 - len(str(bs['cpu2-vp']))) * ' ' + '|')
-
-        print("|[- 3PV -]|   ", end="")
-        print(
-            f"{bs['shop00-pv']}  {self.str_card_bonus(bs['shop00-bonus'])}   ", end="")
-        print(
-            f"{bs['shop10-pv']}  {self.str_card_bonus(bs['shop10-bonus'])}   ", end="")
-        print(
-            f"{bs['shop20-pv']}  {self.str_card_bonus(bs['shop20-bonus'])}   ", end="")
-        print(
-            f"{bs['shop30-pv']}  {self.str_card_bonus(bs['shop30-bonus'])}  ", end="")
-        print(f"| {self.str_token_array(bs['cpu2-tokens'])} |")
-
-        print(f"|{self.str_patron_price(bs['patrons'][1])}|  ", end="")
-        print(f"{self.str_first_card_price(bs['shop00-price'])}  ", end="")
-        print(f"{self.str_first_card_price(bs['shop10-price'])}  ", end="")
-        print(f"{self.str_first_card_price(bs['shop20-price'])}  ", end="")
-        print(f"{self.str_first_card_price(bs['shop30-price'])}  ", end="")
-        print(
-            f"| {self.str_bonus_array(bs['cpu2-bonus'])} [{bs['cpu2-nbReserved']}]|")
-
-        print("|         |  ", end="")
-        print(f"{self.str_scnd_card_price(bs['shop00-price'])}  ", end="")
-        print(f"{self.str_scnd_card_price(bs['shop10-price'])}  ", end="")
-        print(f"{self.str_scnd_card_price(bs['shop20-price'])}  ", end="")
-        print(f"{self.str_scnd_card_price(bs['shop30-price'])}  ", end="")
-        print("+--------------------+")
-
-        print("|[- 3PV -]|                                  | CPU#1        VP:", end='')
-        print(str(bs['cpu3-vp']) + (3 - len(str(bs['cpu3-vp']))) * ' ' + '|')
-
-        print(f"|{self.str_patron_price(bs['patrons'][2])}|   ", end="")
-        print(
-            f"{bs['shop01-pv']}  {self.str_card_bonus(bs['shop01-bonus'])}   ", end="")
-        print(
-            f"{bs['shop11-pv']}  {self.str_card_bonus(bs['shop11-bonus'])}   ", end="")
-        print(
-            f"{bs['shop21-pv']}  {self.str_card_bonus(bs['shop21-bonus'])}   ", end="")
-        print(
-            f"{bs['shop31-pv']}  {self.str_card_bonus(bs['shop31-bonus'])}  ", end="")
-        print(f"| {self.str_token_array(bs['cpu3-tokens'])} |")
-
-        print("|         |  ", end="")
-        print(f"{self.str_first_card_price(bs['shop01-price'])}  ", end="")
-        print(f"{self.str_first_card_price(bs['shop11-price'])}  ", end="")
-        print(f"{self.str_first_card_price(bs['shop21-price'])}  ", end="")
-        print(f"{self.str_first_card_price(bs['shop31-price'])}  ", end="")
-        print(
-            f"| {self.str_bonus_array(bs['cpu3-bonus'])} [{bs['cpu3-nbReserved']}]|")
-
-        print("|[- 3PV -]|  ", end="")
-        print(f"{self.str_scnd_card_price(bs['shop01-price'])}  ", end="")
-        print(f"{self.str_scnd_card_price(bs['shop11-price'])}  ", end="")
-        print(f"{self.str_scnd_card_price(bs['shop21-price'])}  ", end="")
-        print(f"{self.str_scnd_card_price(bs['shop31-price'])}  ", end="")
-        print("+--------------------+")
-
-        print(
-            f"|{self.str_patron_price(bs['patrons'][3])}|                                  |                    |")
-
-        print("|         |   ", end="")
-        print(
-            f"{bs['shop02-pv']}  {self.str_card_bonus(bs['shop02-bonus'])}   ", end="")
-        print(
-            f"{bs['shop12-pv']}  {self.str_card_bonus(bs['shop12-bonus'])}   ", end="")
-        print(
-            f"{bs['shop22-pv']}  {self.str_card_bonus(bs['shop22-bonus'])}   ", end="")
-        print(
-            f"{bs['shop32-pv']}  {self.str_card_bonus(bs['shop32-bonus'])}  ", end="")
-        print("| PLAYER       VP:", end='')
-        print(str(bs['player-vp']) +
-              (3 - len(str(bs['player-vp']))) * ' ' + '|')
-
-        print("|[- 3PV -]|  ", end="")
-        print(f"{self.str_first_card_price(bs['shop02-price'])}  ", end="")
-        print(f"{self.str_first_card_price(bs['shop12-price'])}  ", end="")
-        print(f"{self.str_first_card_price(bs['shop22-price'])}  ", end="")
-        print(f"{self.str_first_card_price(bs['shop32-price'])}  ", end="")
-        print(f"| {self.str_token_array(bs['player-tokens'])} |")
-
-        print(f"|{self.str_patron_price(bs['patrons'][4])}|  ", end="")
-        print(f"{self.str_scnd_card_price(bs['shop02-price'])}  ", end="")
-        print(f"{self.str_scnd_card_price(bs['shop12-price'])}  ", end="")
-        print(f"{self.str_scnd_card_price(bs['shop22-price'])}  ", end="")
-        print(f"{self.str_scnd_card_price(bs['shop32-price'])}  ", end="")
-        print(
-            f"| {self.str_bonus_array(bs['player-bonus'])} [{bs['player-nbReserved']}]|")
-
-        print("|         |                                  |                    |")
-        print("+---------+----------------------------------+--------------------+")
-
-    def str_token_array(self, ta: TokenArray) -> str:
-        assert isinstance(ta, TokenArray)
-        out = ""
-        out += c(f'({ta.get_tokens()[0]})', 'white')
-        out += c(f'({ta.get_tokens()[1]})', 'blue')
-        out += c(f'({ta.get_tokens()[2]})', 'green')
-        out += c(f'({ta.get_tokens()[3]})', 'red')
-        out += c(f'({ta.get_tokens()[4]})', 'light_grey')
-        out += c(f'({ta.get_tokens()[5]})', 'yellow')
-        return out
-
-    def str_bonus_array(self, ta: TokenArray) -> str:
-        out = ""
-        out += c(f'[{ta.get_tokens()[0]}]', 'white')
-        out += c(f'[{ta.get_tokens()[1]}]', 'blue')
-        out += c(f'[{ta.get_tokens()[2]}]', 'green')
-        out += c(f'[{ta.get_tokens()[3]}]', 'red')
-        out += c(f'[{ta.get_tokens()[4]}]', 'light_grey')
-        return out
-
-    def str_patron_price(self, ta: TokenArray) -> str:
-        out = ""
-        count = 0
-        for i, color in enumerate(['white', 'blue', 'green', 'red', 'light_grey']):
-            if ta.get_tokens()[i] != 0:
-                count += 1
-                out += c(f'[{ta.get_tokens()[i]}]', color)
-        out += (9 - 3 * count) * " "
-        return out
-
-    def str_first_card_price(self, ta: TokenArray) -> str:
-        out = ""
-        count = 0
-        for i, color in enumerate(['white', 'blue', 'green', 'red', 'light_grey']):
-            if ta.get_tokens()[i] != 0:
-                out += c(f'({ta.get_tokens()[i]})', color)
-                count += 1
-            if count >= 2:
+            if user_input in self.ESCAPE:
                 break
-        out += (6 - 3 * count) * " "
+            elif user_input[0] in self.COMMANDS.keys():
+                self.COMMANDS[user_input[0]](user_input[1:])
+            else:
+                pass
+
+    def get_input(self, message="") -> list[str]:
+        self.inputWin.display(message)
+
+        out = ''
+        newchar = curses.wget_wch(self.inputWin.win)
+        pos_cursor = 0
+        while newchar != '\n':
+            if type(newchar) == int:
+                if newchar == 263:  # delete
+                    if len(out) > 0 and pos_cursor > 0:
+                        out = out[:pos_cursor - 1] + out[pos_cursor:]
+                        pos_cursor -= 1
+                elif newchar == 260:  # left arrow
+                    pos_cursor = pos_cursor - 1 if pos_cursor > 0 else 0
+                elif newchar == 261:  # right arrow
+                    pos_cursor = pos_cursor + 1 if pos_cursor < len(out) else pos_cursor
+                elif newchar == 258:  # down arrow
+                    pos_cursor = len(out)
+                elif newchar == 259:  # up arrow
+                    pos_cursor = 0
+                else:
+                    with open('log.txt', 'a') as file:
+                        file.write(str(type(newchar)) + '     ' + str(newchar) + '\n')
+            else:
+                out = out[:pos_cursor] + newchar + out[pos_cursor:]
+                pos_cursor += 1
+
+            self.inputWin.display(out[:pos_cursor] + '_' + out[pos_cursor:])
+            newchar = curses.wget_wch(self.inputWin.win)
+
+        self.inputWin.display('')
         return out
 
-    def str_scnd_card_price(self, ta: TokenArray) -> str:
-        out = ""
-        count = 0
-        for i, color in enumerate(['white', 'blue', 'green', 'red', 'light_grey']):
-            if ta.get_tokens()[i] != 0:
-                count += 1
-                if count > 2:
-                    out += c(f'({ta.get_tokens()[i]})', color)
-        if count == 1:
-            out += 6 * " "
-        else:
-            out += (6 - 3 * (count - 2)) * " "
-        return out
+    def display(self) -> None:
+        curses.werase(self.stdscr)
 
-    def str_card_bonus(self, ta: TokenArray) -> str:
-        for i, color in enumerate(['white', 'blue', 'green', 'red', 'light_grey']):
-            if ta.get_tokens()[i] != 0:
-                return c('[]', color)
-        return 'er'
+        # border
+        right = 67
+        bottom = 21
+        curses.mvwhline(self.stdscr, 0, 0, curses.ACS_HLINE, right)
+        curses.mvwhline(self.stdscr, bottom, 0, curses.ACS_HLINE, right)
+        curses.mvwvline(self.stdscr, 0, 0, curses.ACS_VLINE, bottom)
+        curses.mvwvline(self.stdscr, 0, right, curses.ACS_VLINE, bottom)
+        curses.mvwaddch(self.stdscr, 0, 0, curses.ACS_ULCORNER)
+        curses.mvwaddch(self.stdscr, 0, right, curses.ACS_URCORNER)
+        curses.mvwaddch(self.stdscr, bottom, 0, curses.ACS_LLCORNER)
+        curses.mvwaddch(self.stdscr, bottom, right, curses.ACS_LRCORNER)
+
+        # patron windows
+        self.patronWins = [PatronWin(patron, y, 0) for patron, y in zip(self.gm.patronController.patrons, (3, 8, 13, 18, 23))]
+        for patronWin in self.patronWins:
+            patronWin.display()
+
+        # card windows
+        self.cardWins = []
+        for i, rank in enumerate(self.gm.shopController.ranks):
+            for j, card in enumerate(rank.hand.cards):
+                self.cardWins.append(CardWin(card, 16 - 6 * i, 12 + j * 8))
+                self.cardWins[-1].display()
+
+        for i, card in enumerate(self.gm.playerController.players[self.gm.userId].reserved.cards):
+            self.cardWins.append(CardWin(card, 10 + 6 * i, 47))
+            self.cardWins[-1].display()
+
+        # player windows
+        self.playerWin = []
+        for i, player in enumerate(self.gm.playerController.players):
+            if player.player_id == self.gm.userId:
+                name = 'Player'
+            else:
+                name = f'CPU#{player.player_id}'
+            self.playerWin.append(PlayerWin(player, name, i * 6, 45))
+            self.playerWin[-1].display()
+
+        # input window
+        self.inputWin.display()
+
+    def take_tokens(self) -> None:
+        pass
+
+    def reserve(self) -> None:
+        pass
+
+    def buy(self) -> None:
+        pass
+
+    def help(self) -> None:
+        pass
---
+++ model/cli/objects_win.py
@@ -0,0 +1,107 @@
+import unicurses as curses
+from model.patron import Patron
+from model.card import Card
+from model.player import Player
+
+
+class MyWin():
+    colors = (curses.COLOR_WHITE, curses.COLOR_BLUE, curses.COLOR_GREEN, curses.COLOR_RED, curses.COLOR_BLACK, curses.COLOR_YELLOW)
+
+    def draw_border(self):
+        bottom, right = curses.getmaxyx(self.win)
+        curses.mvwhline(self.win, 0, 0, curses.ACS_HLINE, right)
+        curses.mvwhline(self.win, bottom, 0, curses.ACS_HLINE, right)
+        curses.mvwvline(self.win, 0, 0, curses.ACS_VLINE, bottom)
+        curses.mvwvline(self.win, 0, right, curses.ACS_VLINE, bottom)
+        curses.mvwaddch(self.win, 0, 0, curses.ACS_ULCORNER)
+        curses.mvwaddch(self.win, 0, right, curses.ACS_URCORNER)
+        curses.mvwaddch(self.win, bottom, 0, curses.ACS_LLCORNER)
+        curses.mvwaddch(self.win, 0, right, curses.ACS_LRCORNER)
+
+
+class PatronWin(MyWin):
+    def __init__(self, patron: Patron, y, x) -> None:
+        self.patron = patron
+        self.win = curses.newwin(nlines=4, ncols=11, begin_y=y, begin_x=x)
+
+    def display(self) -> None:
+        curses.werase(self.win)
+        self.draw_border()
+
+        curses.mvwaddstr(self.win, 1, 1, ' - 3PV - ')
+
+        requirements = self.patron.requirements.get_tokens()
+        if sum(requirements) == 2:
+            x = 2
+            for i in range(6):
+                if requirements[i] == 4:
+                    curses.mvwaddstr(self.win, 1, x, '[4]', self.colors[i])
+                    x += 6
+        else:
+            x = 1
+            for i in range(6):
+                if requirements[i] == 3:
+                    curses.mvwaddstr(self.win, 1, x, '[3]', self.colors[i])
+                    x += 3
+        curses.wrefresh(self.win)
+
+
+class CardWin(MyWin):
+    def __init__(self, card: Card, y, x) -> None:
+        self.card = card
+        self.win = curses.newwin(nlines=7, ncols=7, begin_y=y, begin_x=x)
+
+    def display(self) -> None:
+        curses.werase(self.win)
+        self.draw_border()
+
+        curses.mvwaddstr(self.win, 1, 1, str(self.card.victoryPoint.value))
+        curses.mvwaddstr(self.win, 1, 4, '[]', self.colors[self.card.bonus.get_tokens().index(1)])
+
+        if len(str(self.card.card_id)) == 1:
+            curses.mvwaddstr(self.win, 2, 1, f'__{self.card.card_id}__')
+        else:
+            curses.mvwaddstr(self.win, 2, 1, f'_{self.card.card_id}__')
+
+        x = 1
+        y = 3
+        for i in range(6):
+            if self.card.price.get_tokens()[i] != 0:
+                curses.mvwaddstr(self.win, y, x, f'({self.card.price.get_tokens()[i]})', self.colors[i])
+                x += 3
+                if x > 4:
+                    x = 1
+                    y += 1
+        curses.wrefresh(self.win)
+
+
+class PlayerWin(MyWin):
+    def __init__(self, player: Player, name: str, y, x) -> None:
+        self.player = player
+        self.name = name
+        self.win = curses.newwin(nlines=5, ncols=22, begin_y=y, begin_x=x)
+
+    def display(self):
+        curses.werase(self.win)
+        self.draw_border()
+
+        curses.mvwaddstr(self.win, 1, 2, self.name)
+        curses.mvwaddstr(self.win, 1, 15, f'VP:{self.player.victoryPoints.value}')
+
+        for i, v in enumerate(self.player.tokens.get_tokens()):
+            curses.mvwaddstr(self.win, 2, 2 + i, f'({v})', self.colors[i])
+
+        for i, v in enumerate(self.player.hand.compute_hand_bonuses().get_tokens()):
+            curses.mvwaddstr(self.win, 3, 2 + i, f'[{v}]', self.colors[i])
+        curses.wrefresh(self.win)
+
+
+class InputWin(MyWin):
+    def __init__(self) -> None:
+        self.win = curses.newwin(3, 67, 20, 0)
+
+    def display(self, message=''):
+        curses.werase(self.win)
+        self.draw_border()
+        curses.mvwaddstr(self.win, 1, 2, message)
+        curses.wrefresh(self.win)
