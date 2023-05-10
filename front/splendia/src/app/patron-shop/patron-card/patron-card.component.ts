242d03f54f9a914461c319381de58d6d3b857cd1
Lucas Bruton <lucas.bruton9@gmail.com>
2023-05-10 23:52:33 +0200

    Continued patrons

--- front/splendia/src/app/patron-shop/patron-card/patron-card.component.ts
+++ front/splendia/src/app/patron-shop/patron-card/patron-card.component.ts
@@ -1,33 +1,20 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { OnDestroyMixin, untilComponentDestroyed } from '@w11k/ngx-componentdestroyed';
-import { BoardState } from 'src/app/shared/models/board_state.model';
 import { Patron } from 'src/app/shared/models/patron.model';
-import { AppService } from 'src/app/shared/services/app.service';
+
 
 @Component({
   selector: 'app-patron-card',
   templateUrl: './patron-card.component.html',
   styleUrls: ['./patron-card.component.scss']
 })
-export class PatronCardComponent extends OnDestroyMixin implements OnInit {
-url(arg0: string): any {
-throw new Error('Method not implemented.');
-}
-  // Corresponds to the position of the patron card in board_state.patrons
-  @Input() patronNumberList:number;
-  // Patron to display
-  patron: Patron;
+export class PatronCardComponent implements OnInit {
 
-  constructor(private appService: AppService) { 
-    super();
-  }
+  // Patron to display
+  @Input() patron: Patron;
+
+  constructor() {}
 
   ngOnInit(): void {
-    // Subscribe to board_state.patrons[this.patronNumberList]
-    this.appService.board_state.pipe(untilComponentDestroyed(this)).subscribe((board_state:BoardState) => {
-      this.patron = board_state.patrons[this.patronNumberList];
-      console.log(this.patron.patronId);
-    });
   }
 
 }
--- front/splendia/src/app/patron-shop/patron-shop/patron-shop.component.ts
+++ front/splendia/src/app/patron-shop/patron-shop/patron-shop.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { OnDestroyMixin, untilComponentDestroyed } from '@w11k/ngx-componentdestroyed';
+import { BoardState } from 'src/app/shared/models/board_state.model';
+import { Patron } from 'src/app/shared/models/patron.model';
+import { AppService } from 'src/app/shared/services/app.service';
 
 @Component({
   selector: 'app-patron-shop',
   templateUrl: './patron-shop.component.html',
   styleUrls: ['./patron-shop.component.scss']
 })
-export class PatronShopComponent implements OnInit {
+export class PatronShopComponent extends OnDestroyMixin implements OnInit {
 
-  constructor() { }
+  // Patrons to display
+  patrons: Patron[];
 
-  ngOnInit(): void {
+  constructor(private appService: AppService) { 
+    super();
   }
 
+  ngOnInit(): void {
+    // Subscribe to board_state.patrons
+    this.appService.board_state.pipe(untilComponentDestroyed(this)).subscribe((board_state:BoardState) => {
+      this.patrons = board_state.patrons;
+    });
+  }
 }
