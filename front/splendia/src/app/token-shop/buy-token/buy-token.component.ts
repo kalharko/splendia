cb9ef8c8faf4465a240dadd9914542e9c5bf9c63
kalharko <oscar.dewasmes@gmail.com>
2023-06-09 16:49:39 +0200

    :children_crossing: better display of take token

--- front/splendia/src/app/token-shop/buy-token/buy-token.component.ts
+++ front/splendia/src/app/token-shop/buy-token/buy-token.component.ts
@@ -12,12 +12,14 @@ export class BuyTokenComponent implements OnInit {
 
   tokensToBuy: number[];
   tokenList: number[];
+  realTokenList: number[];
 
   constructor(private appService: AppService,
     public dialogRef: MatDialogRef<BuyTokenComponent>,
     @Inject(MAT_DIALOG_DATA) public data: {tokenList: number[]}) {
 
     this.tokensToBuy = [0, 0, 0, 0, 0, 0];
+    this.realTokenList = data.tokenList;
     this.tokenList = data.tokenList;
   }
 
@@ -25,14 +27,16 @@ export class BuyTokenComponent implements OnInit {
   }
 
   buyToken(i: number) : void {
-    if (this.tokenList[i] > this.tokensToBuy[i]) {
+    if (this.realTokenList[i] > this.tokensToBuy[i]) {
       ++this.tokensToBuy[i];
+      --this.tokenList[i];
     }
   }
 
   cancelToken(i: number) : void {
     if (this.tokensToBuy[i] > 0) {
       --this.tokensToBuy[i];
+      ++this.tokenList[i];
     }
   }
 
